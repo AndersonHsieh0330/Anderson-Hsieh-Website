e96a46df92ba12e967137815ed482beaa6d6eccb
Anderson Hsieh <andy13579246810@gmail.com>
2022-03-22 10:37:04 -0400

    bring back react router again

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import AboutPage from './screens/AboutMe.js';
 import TechPage from './screens/Tech.js';
 import BottomMenu from './components/BottomMenu.js';
 import ContactPage from './screens/Contact.js'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 function App() {
   return (
     
-    <div className='Canvas'>
+    <div className='App' >
+      <Routes>
+        <Route path = "/" element = {<AboutPage/>} />
+        <Route path = "/tech"element = {<TechPage/>} />
+        <Route path = "/contact" element = {<ContactPage/>} />
+        </Routes>
+      
+      <h1>h</h1>
       <BottomMenu/>
     </div>
+    
     
   );
 }
